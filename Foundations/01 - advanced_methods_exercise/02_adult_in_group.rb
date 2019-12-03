c1de7771fb9d7fbdb906e7657d0dce66010b6858
robinbshrestha <robinbshrestha@gmail.com>
2019-12-02 19:42:01 -0500

    answers to advanced methods exercise

--- Foundations/01 - advanced_methods_exercise/02_adult_in_group.rb
+++ Foundations/01 - advanced_methods_exercise/02_adult_in_group.rb
@@ -2,6 +2,11 @@
 # The method should return true if there is at least 1 person with an age of 18 or greater.
 # The method should return false otherwise.
 
+def adult_in_group?(people)
+    people.any? do |hash|
+        hash[:age] >= 18
+    end
+end
 
 people_1 = [
     {name: "Jack", age: 17},
--- Foundations/01 - advanced_methods_exercise/04_filter_lengths.rb
+++ Foundations/01 - advanced_methods_exercise/04_filter_lengths.rb
@@ -3,6 +3,17 @@
 # that have at least the given length. The length argument should be optional; if no length
 # is passed in, then 5 should be used as the length.
 
+def filter_lengths(strings, length=5)
+    ans = []
+    strings.each do |string|
+        if string.length >= length 
+            ans << string
+        end
+    end
+
+    return ans
+
+end
 
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
