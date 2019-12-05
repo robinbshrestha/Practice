c7f2e669365051fd9a509072c3a58d36c7a48237
robinbshrestha <robinbshrestha@gmail.com>
2019-12-05 08:45:20 -0500

    complete debugging exercise 2

--- Foundations/07 - debugging_exercise_2/lib/problems.rb
+++ Foundations/07 - debugging_exercise_2/lib/problems.rb
@@ -33,3 +33,62 @@ def is_prime?(num)
     end
     true
 end
+
+def unique_chars?(str)
+    letters = str.split("")
+    hash = Hash.new(0)
+
+    letters.each do |letter|
+        hash[letter] += 1
+    end
+
+    hash.each do |k, v|
+        if v > 1
+            return false 
+        end
+    end
+
+    true
+end
+
+def dupe_indices(arr)
+    hash = Hash.new(0)
+    other_hash = Hash.new{|h, k| h[k] = []}
+
+    arr.each do |char|
+        hash[char] += 1
+    end
+
+
+    hash.each do |k, v|
+        if v > 1
+            i = 0
+            while i < arr.length
+                if arr[i] == k
+                    other_hash[k].push(i)
+                end
+                i += 1
+            end
+        end
+    end
+
+    other_hash
+
+
+end
+
+def ana_array(arr1, arr2)
+    hash1 = Hash.new(0)
+    hash2 = Hash.new(0)
+
+    arr1.each do |letter|
+        hash1[letter] += 1 
+    end
+
+    arr2.each do |letter|
+        hash2[letter] += 1
+    end
+
+    hash1 == hash2
+
+end
